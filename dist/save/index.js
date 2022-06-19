6f90a3d0719e1b335637759e67fd151cd0c3c8e8
Andrei <andrei.evguenov@gmail.com>
2022-06-20 00:03:39 +1200

    Check

--- dist/save/index.js
+++ dist/save/index.js
@@ -61708,8 +61708,24 @@ var external_fs_default = /*#__PURE__*/__nccwpck_require__.n(external_fs_);
 function clientOverride() {
     return {
         ...(0,cache.defaultCacheClient)(),
+        getCacheEntry: async (key, paths, options) => {
+            core.notice('Check');
+            core.notice('\u001b[31;46mRed foreground with a cyan background and \u001b[1mbold text at the end');
+            core.notice('http://google.com');
+            core.info(JSON.stringify({ from: "getCacheEntry", key, paths, options }));
+            return {
+                cacheKey: "abc",
+                archiveLocation: "location",
+            };
+        },
+        downloadCache: async (archiveLocation, archivePath, options) => {
+            core.notice('Check');
+            core.notice('\u001b[31;46mRed foreground with a cyan background and \u001b[1mbold text at the end');
+            core.notice('http://google.com');
+            core.info(JSON.stringify({ from: "downloadCache", archiveLocation, archivePath, options }));
+        },
         reserveCache: async (key, string, paths) => {
-            core.info(JSON.stringify({ updated: "updated", key, string, paths }));
+            core.info(JSON.stringify({ from: "reserveCache", key, string, paths }));
             return {
                 statusCode: 200,
                 result: { cacheId: 2 },
@@ -61718,8 +61734,9 @@ function clientOverride() {
         },
         saveCache: async (cacheId, archivePath, options) => {
             core.notice('Check');
-            core.info('\u001b[31;46mRed foreground with a cyan background and \u001b[1mbold text at the end');
-            core.info(JSON.stringify({ updated: "updated", cacheId, archivePath, options }));
+            core.notice('\u001b[31;46mRed foreground with a cyan background and \u001b[1mbold text at the end');
+            core.notice('http://google.com');
+            core.info(JSON.stringify({ from: "saveCache", cacheId, archivePath, options }));
         }
     };
 }
--- src/cacheClient.ts
+++ src/cacheClient.ts
@@ -4,8 +4,25 @@ import * as core from "@actions/core";
 export function clientOverride(): CacheClient {
     return {
         ...defaultCacheClient(),
+        getCacheEntry: async(key, paths, options) => {
+            core.notice('Check')
+            core.notice('\u001b[31;46mRed foreground with a cyan background and \u001b[1mbold text at the end');
+            core.notice('http://google.com');
+            core.info(JSON.stringify({ from: "getCacheEntry",key, paths, options }));
+            return {
+                cacheKey: "abc",
+                archiveLocation: "location",
+            }
+        },
+        downloadCache: async(archiveLocation, archivePath, options?) => {
+            core.notice('Check')
+            core.notice('\u001b[31;46mRed foreground with a cyan background and \u001b[1mbold text at the end');
+            core.notice('http://google.com');
+            core.info(JSON.stringify({ from: "downloadCache", archiveLocation,archivePath, options }));
+      
+        },
         reserveCache: async(key, string, paths) => {
-            core.info(JSON.stringify({ updated: "updated", key, string, paths }));
+            core.info(JSON.stringify({ from: "reserveCache", key, string, paths }));
             return {
                 statusCode: 200,
                 result: { cacheId: 2},
@@ -16,7 +33,7 @@ export function clientOverride(): CacheClient {
             core.notice('Check')
             core.notice('\u001b[31;46mRed foreground with a cyan background and \u001b[1mbold text at the end');
             core.notice('http://google.com');
-            core.info(JSON.stringify({ updated: "updated", cacheId, archivePath, options }));
+            core.info(JSON.stringify({from: "saveCache", cacheId, archivePath, options }));
         }
     };
 }
